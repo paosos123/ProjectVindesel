b587506a11e38a62dd3993505ec5fb76724ca560
paosos123 <paosos123@gmail.com>
2025-03-11 21:08:45 +0700

    Movement ingame
    Have Dash And Movement

---
+++ Assets/Scripts/Movement2D.cs
@@ -0,0 +1,241 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//////////SETUP INSTRUCTIONS//////////
+//Attach this script a RigidBody2D to the player GameObject
+//Set Body type to Dynamic, Collision detection to continuous and Freeze Z rotation
+//Add a 2D Collider (Any will do, but 2D box collider)
+//Define the ground and wall mask layers (In the script and in the GameObjects)
+//Adjust and play around with the other variables (Some require you to activate gizmos in order to visualize)
+
+public class Movement2D : MonoBehaviour
+{
+    [Header("Components")]
+    private Rigidbody2D _rb;
+    private Animator _anim;
+
+    [Header("Layer Masks")]
+    [SerializeField] private LayerMask layerMask;
+    
+
+    [Header("Movement Variables")]
+    [SerializeField] private float _movementAcceleration = 70f;
+    [SerializeField] private float _maxMoveSpeed = 12f;
+    [SerializeField] private float _groundLinearDrag = 7f;
+    private float _horizontalDirection;
+    private float _verticalDirection;
+    private bool _changingDirection => (_rb.velocity.x > 0f && _horizontalDirection < 0f) || (_rb.velocity.x < 0f && _horizontalDirection > 0f);
+    private bool _facingRight = true;
+
+    [Header("Jump Variables")]
+    [SerializeField] private float _jumpForce = 12f;
+    [SerializeField] private float _airLinearDrag = 2.5f;
+    [SerializeField] private float _fallMultiplier = 8f;
+    [SerializeField] private float _lowJumpFallMultiplier = 5f;
+    [SerializeField] private float _downMultiplier = 12f;
+    [SerializeField] private int _extraJumps = 1;
+    [SerializeField] private float _hangTime = .1f;
+    [SerializeField] private float _jumpBufferLength = .1f;
+    private int _extraJumpsValue;
+    private float _hangTimeCounter;
+    private float _jumpBufferCounter;
+    private bool _canJump => _jumpBufferCounter > 0f && (_hangTimeCounter > 0f || _extraJumpsValue > 0 );
+    private bool _isJumping = false;
+    
+
+    [Header("Dash Variables")]
+    [SerializeField] private float _dashSpeed = 15f;
+    [SerializeField] private float _dashLength = .3f;
+    [SerializeField] private float _dashBufferLength = .1f;
+    private float _dashBufferCounter;
+    private bool _isDashing;
+    private bool _hasDashed;
+    private bool _canDash => _dashBufferCounter > 0f && !_hasDashed;
+
+    [Header("Ground Collision Variables")]
+    [SerializeField] private float distanceGroundCheck ;
+    [SerializeField] private Vector2 boxSize;
+   
+    
+    
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _horizontalDirection = GetInput().x;
+        _verticalDirection = GetInput().y;
+        if (Input.GetButtonDown("Jump")) _jumpBufferCounter = _jumpBufferLength;
+        else _jumpBufferCounter -= Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.E)) _dashBufferCounter = _dashBufferLength;
+        else _dashBufferCounter -= Time.deltaTime;
+        
+        FlipController();
+    }
+
+    private void FixedUpdate()
+    {
+       
+        if (_canDash) StartCoroutine(Dash(_horizontalDirection, _verticalDirection));
+        if (!_isDashing)
+        {
+            MoveCharacter();
+            if (isGrounded())
+            {
+                ApplyGroundLinearDrag();
+                _extraJumpsValue = _extraJumps;
+                _hangTimeCounter = _hangTime;
+                _hasDashed = false;
+                 
+            }
+            else
+            {
+                ApplyAirLinearDrag();
+                FallMultiplier();
+                _hangTimeCounter -= Time.fixedDeltaTime;
+                if ( _rb.velocity.y < 0f ) _isJumping = false;
+            }
+          
+        }
+
+        if (_canJump)
+        {
+            Jump(Vector2.up);
+        }
+        
+    }
+
+    private Vector2 GetInput()
+    {
+        return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void MoveCharacter()
+    {
+        _rb.AddForce(new Vector2(_horizontalDirection, 0f) * _movementAcceleration);
+
+        if (Mathf.Abs(_rb.velocity.x) > _maxMoveSpeed)
+            _rb.velocity = new Vector2(Mathf.Sign(_rb.velocity.x) * _maxMoveSpeed, _rb.velocity.y);
+    }
+
+    private void ApplyGroundLinearDrag()
+    {
+        if (Mathf.Abs(_horizontalDirection) < 0.4f || _changingDirection)
+        {
+            _rb.drag = _groundLinearDrag;
+        }
+        else
+        {
+            _rb.drag = 0f;
+        }
+    }
+
+    private void ApplyAirLinearDrag()
+    {
+         _rb.drag = _airLinearDrag;
+    }
+
+    private void Jump(Vector2 direction)
+    {
+        if (!isGrounded())
+            _extraJumpsValue--;
+
+        ApplyAirLinearDrag();
+        _rb.velocity = new Vector2(_rb.velocity.x, 0f);
+        _rb.AddForce(direction * _jumpForce, ForceMode2D.Impulse);
+        _hangTimeCounter = 0f;
+        _jumpBufferCounter = 0f;
+        _isJumping = true;
+    }
+
+   
+
+   
+    
+    private void FallMultiplier()
+    {
+        if (_verticalDirection < 0f)
+        {
+            _rb.gravityScale = _downMultiplier;
+        }
+        else
+        {
+            if (_rb.velocity.y < 0)
+            {
+                _rb.gravityScale = _fallMultiplier;
+            }
+            else if (_rb.velocity.y > 0 && !Input.GetButton("Jump"))
+            {
+                _rb.gravityScale = _lowJumpFallMultiplier;
+            }
+            else
+            {
+                _rb.gravityScale = 1f;
+            }
+        }
+    }
+
+    void FlipController()
+    {
+        if(_rb.velocity.x < 0f &&  _facingRight)
+            Flip();
+        else if(_rb.velocity.x > 0f && !_facingRight)
+            Flip();
+    }
+   
+
+    void Flip()
+    {
+        _facingRight = !_facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+    IEnumerator Dash(float x, float y)
+    {
+        float dashStartTime = Time.time;
+        _hasDashed = true;
+        _isDashing = true;
+        _isJumping = false;
+
+        _rb.velocity = Vector2.zero;
+        _rb.gravityScale = 0f;
+        _rb.drag = 0f;
+
+        Vector2 dir;
+        if (x != 0f || y != 0f) dir = new Vector2(x,y);
+        else
+        {
+            if (_facingRight) dir = new Vector2(1f, 0f);
+            else dir = new Vector2(-1f, 0f);
+        }
+
+        while (Time.time < dashStartTime + _dashLength)
+        {
+            _rb.velocity = dir.normalized * _dashSpeed;
+            yield return null;
+        }
+
+        _isDashing = false;
+    }
+    private bool isGrounded()
+    {
+        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, distanceGroundCheck,layerMask))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position-transform.up*distanceGroundCheck, boxSize);
+    }
+}
